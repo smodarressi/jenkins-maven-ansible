b7dcbefdc456f8738ac64ba62f53aec68d502b52
smodarressi <38509968+smodarressi@users.noreply.github.com>
2021-06-13 17:23:18 -0700

    Update DemoApplicationTests.java

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class DemoApplication {
+public class DemoApplicationTests {
 
 public static void main(String[] args) {
 SpringApplication.run(DemoApplication.class, args);
